436a5fb23c35d56f06923349283c86917d978d26
TWang <wthsylor@gmail.com>
2019-03-09 20:44:30 -0800

    finished asisgnment 4

--- assignment4/TianheWang_Assignment4.R
+++ assignment4/TianheWang_Assignment4.R
@@ -4,9 +4,8 @@ library(dplyr)
 library(htmltools)
 library(tidyr)
 library(ggplot2)
-library(expss)
 
-setwd("/Users/tianhewang/Desktop/ANLY512/Assignment\ 4")
+setwd("D:\\Work\\HU\\dataviz\\assignment4")
 allSchools <- read.csv("EasternSchools.csv", quote = "\"", header = TRUE)
 allSchools <- mutate(allSchools, raceOTHERS = allSchools$raceWHITE + allSchools$raceAMERIND + allSchools$raceHI + allSchools$raceBLACK)
 school_lvl1 <- allSchools[which(allSchools$LEVEL==1), ]
@@ -14,14 +13,18 @@ school_lvl2 <- allSchools[which(allSchools$LEVEL==2), ]
 school_lvl3 <- allSchools[which(allSchools$LEVEL==3), ]
 school_lvl4 <- allSchools[which(allSchools$LEVEL==4), ]
 
-lvl1_schools = leaflet() %>% addProviderTiles("CartoDB.DarkMatter") %>% 
-                  addCircleMarkers(data=school_lvl1,lng=~LON,lat=~LAT, color="red", fillColor = "red")
-lvl2_schools = leaflet() %>% addProviderTiles("CartoDB.DarkMatter") %>% 
-                  addCircleMarkers(data=school_lvl2,lng=~LON,lat=~LAT, color="yellow", fillColor = "yellow")
-lvl3_schools = leaflet() %>% addProviderTiles("CartoDB.DarkMatter") %>% 
-                  addCircleMarkers(data=school_lvl3,lng=~LON,lat=~LAT, color="blue", fillColor = "blue")
-lvl4_schools = leaflet() %>% addProviderTiles("CartoDB.DarkMatter") %>% 
-                  addCircleMarkers(data=school_lvl4,lng=~LON,lat=~LAT, color="green", fillColor = "green")
+lvl1_schools = leaflet() %>% addTiles() %>% 
+                  addCircleMarkers(data=school_lvl1,lng=~LON,lat=~LAT, 
+                                   radius = 3, color="red", fillColor = "red")
+lvl2_schools = leaflet() %>% addTiles() %>% 
+                  addCircleMarkers(data=school_lvl2,lng=~LON,lat=~LAT, 
+                                   radius = 3, color="yellow", fillColor = "yellow")
+lvl3_schools = leaflet() %>% addTiles() %>% 
+                  addCircleMarkers(data=school_lvl3,lng=~LON,lat=~LAT, 
+                                   radius = 3, color="blue", fillColor = "blue")
+lvl4_schools = leaflet() %>% addTiles() %>% 
+                  addCircleMarkers(data=school_lvl4,lng=~LON,lat=~LAT, 
+                                   radius = 3, color="green", fillColor = "green")
 
 leaflet_grid <- 
   tagList(
@@ -39,13 +42,30 @@ leaflet_grid <-
 
 browsable(leaflet_grid)
 
-zips<-readOGR(dsn="./cb_2017_us_county_500k",layer="cb_2017_us_county_500k") # no file extension!
+counties <- readOGR(dsn="./Pennsylvania_School_Districts",layer="cb_2016_42_unsd_500k")
+grades01 = school_lvl1$G01
 
-leaflet() %>% addTiles() %>%
-  addCircleMarkers(data = a, radius = 3, color = "blue", weight = 3, opacity = 1, fill = TRUE, fillColor = "blue", fillOpacity = 0.2, label = ~city) %>%
-  addCircleMarkers(data = b, radius = 3, color = "yellow", weight = 3, opacity = 0.5, fill = TRUE, fillColor = "red", fillOpacity = 0.2, label = ~city) %>%
-  addRectangles(lng1 = -75.00000, lat1 = 39.90000, lng2 = -75.30000, lat2 = 40.10000, color = "red", weight = 5,
+G01_grades = leaflet(counties) %>% addTiles() %>%
+  addCircleMarkers(data = school_lvl1, radius = 1.5, fill = FALSE, color = "red", opacity = (grades01-20)/100,
+                   lng=~LON, lat=~LAT) %>%
+  addRectangles(lng1 = -74.90000, lat1 = 40.10000, lng2 = -75.40000, lat2 = 40.35000, color = "black", weight = 5,
                 fillColor = "transparent") %>%
-  addLabelOnlyMarkers(lng = -75.1652, lat = 39.9526, label = "Philadelphia", 
-                      labelOptions = labelOptions(noHide = T, textsize = "10px")) %>%
-  addLegend("topright", colors = c("blue", "yellow"), labels = c("Magnet School", "Charter School"), title = "Type of Speciality School", opacity = 1)
+  addRectangles(lng1 = -75.70000, lat1 = 40.50000, lng2 = -75.10000, lat2 = 40.75000, color = "black", weight = 5,
+                fillColor = "transparent")
+G01_grades
+
+
+m_school = allSchools %>% filter(magnetSchoolStatus == "Yes")
+c_school = allSchools %>% filter(isCHARTER == "Yes")
+
+school_grade = leaflet(counties) %>% addTiles() %>%
+  addPolygons(weight=1,color="blue",fillOpacity = .1) %>%
+  addCircleMarkers(data = m_school, radius = 2, color = "yellow", weight = 3, opacity = (grades01)/100, label = ~city) %>%
+  addCircleMarkers(data = c_school, radius = 2, color = "blue", weight = 3, opacity = (grades01)/100, label = ~city) %>%
+  addLegend("topright", colors = c("red", "green"), labels = c("magnet school", "charter school"), title = "Type of School", opacity = 1)
+school_grade
+
+asians = leaflet() %>% addTiles() %>% 
+  addCircleMarkers(data=allSchools,lng=~LON,lat=~LAT, 
+                   radius = 3, opacity = allSchools$raceASIAN / 100, color="purple", fillColor = "purple")
+asians
